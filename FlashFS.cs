cd1e14c98adaefcccd38125dc97edfb84f7fa983
TitleOS <titleos@titleos.dev>
2021-07-07 20:28:16 -0500

    Add project files.

---
+++ FlashFS.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantumTunnel
+{
+    public static class FlashFS
+    {
+        private static readonly string FlashDeviceName = "\\\\.\\Xvuc\\FlashFs";
+
+        public static void ReadFile(string name)
+        {
+            string fullpath = FlashDeviceName + "\\" + name;
+            IntPtr FileHandle = KernelBase.CreateFile(fullpath, System.IO.FileAccess.Read, System.IO.FileShare.None, IntPtr.Zero, System.IO.FileMode.Open, System.IO.FileAttributes.Normal, IntPtr.Zero);
+            FileStream FlashStream = new FileStream(FileHandle, FileAccess.Read);
+            byte[] b = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int count = 0;
+                do
+                {
+                    byte[] buf = new byte[1024];
+                    count = FlashStream.Read(buf, 0, 1024);
+                    ms.Write(buf, 0, count);
+                } while (FlashStream.CanRead && count > 0);
+                b = ms.ToArray();
+                File.WriteAllBytes(name, b);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace QuantumTunnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("QuantumTunnel - C# FlashFS Reader");
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Provide name of file on flash. Example: certkeys.bin");
+                Environment.Exit(-1);
+            }
+            string filename = args[0];
+            FlashFS.ReadFile(filename);
+            Console.WriteLine($"File read to {Environment.CurrentDirectory}\\{filename}");
+        }
+    }
+}
