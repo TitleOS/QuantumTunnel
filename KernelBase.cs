e4c11b8ddab659f1e2994e3267b219a56ba9c78e
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-02-21 06:31:47 +0100

    Fix both, Flash and FlashFs dumping

--- KernelBase.cs
+++ KernelBase.cs
@@ -13,12 +13,23 @@ namespace QuantumTunnel
     {
         [DllImport("kernelbase.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern IntPtr CreateFile(
-     [MarshalAs(UnmanagedType.LPWStr)] string filename,
-     [MarshalAs(UnmanagedType.U4)] FileAccess access,
-     [MarshalAs(UnmanagedType.U4)] FileShare share,
-     IntPtr securityAttributes,
-     [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,
-     [MarshalAs(UnmanagedType.U4)] FileAttributes flagsAndAttributes,
-     IntPtr templateFile);
+        [MarshalAs(UnmanagedType.LPWStr)] string filename,
+        [MarshalAs(UnmanagedType.U4)] FileAccess access,
+        [MarshalAs(UnmanagedType.U4)] FileShare share,
+        IntPtr securityAttributes,
+        [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,
+        [MarshalAs(UnmanagedType.U4)] FileAttributes flagsAndAttributes,
+        IntPtr templateFile);
+
+        [DllImport("kernelbase.dll", SetLastError=true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernelbase.dll", SetLastError = true)]
+        public static extern bool ReadFile(IntPtr hFile, [Out] byte[] lpBuffer,
+            uint nNumberOfBytesToRead, out uint lpNumberOfBytesRead, IntPtr lpOverlapped);
+
+        [DllImport("kernelbase.dll")]
+        public static extern uint GetLastError();
     }
 }
